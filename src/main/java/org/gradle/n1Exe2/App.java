7ec050a6312246227d5f87274f277bcd66ef31d3
plopezgit <p.lopez.ch.hr@hotmail.com>
2023-11-05 23:22:44 +0100

    creates intial App class for the abstract factory uses

---
+++ src/main/java/org/gradle/n1Exe2/App.java
@@ -0,0 +1,17 @@
+package org.gradle.n1Exe2;
+
+public class App {
+	
+	public static void main (String[] args) {
+		
+		/*
+		- Level 2
+			**Abstract Factory**
+			Create a small manager of international phone numbers and addresses.
+			The application must allow international addresses and phone numbers 
+			to be added to the address book. Considering the different formats of 
+			different countries, it builds the phone book, addresses and phones by 
+			implementing an Abstract Factory pattern.
+		*/
+	}
+}
