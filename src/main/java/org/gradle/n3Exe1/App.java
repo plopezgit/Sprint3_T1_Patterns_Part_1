a2ae6f8e46af05e801de6e36405ad59593d0e8fa
plopezgit <p.lopez.ch.hr@hotmail.com>
2023-11-07 12:49:24 +0100

    initial commit

---
+++ src/main/java/org/gradle/n3Exe1/App.java
@@ -0,0 +1,14 @@
+package org.gradle.n3Exe1;
+
+public class App {
+	
+	public static void main (String[] args) {
+		
+		/*
+		Design a parking lot of 4 vehicles: a car, a bicycle, a plane and a boat.
+		It shows how the Command pattern works that implements the start, accelerate, 
+			and brake methods for each vehicle type.
+		*/
+			
+	}
+}
