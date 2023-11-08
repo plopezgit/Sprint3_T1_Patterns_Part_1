5ca6984452ccb7a439ab47d6182a915a6ddadba6
plopezgit <p.lopez.ch.hr@hotmail.com>
2023-11-08 10:45:23 +0100

    check code format

--- src/main/java/org/gradle/n2Exe1/client/App.java
+++ src/main/java/org/gradle/n2Exe1/client/App.java
@@ -15,36 +15,32 @@ public class App {
 	public static void main(String[] args) {
 
 		ContactBook contactBook = ContactBook.getInstance();
-		
+
 		IternationalContactAbstractFactory phoneNumberFactory = GetterFactory.getFactory("phone number");
 		PhoneNumberInterface phoneNumber = phoneNumberFactory.createPhoneNumber("Spain");
 		IternationalContactAbstractFactory addressFactory = GetterFactory.getFactory("address");
 		AddressInterface address = addressFactory.createAddress("Spain");
-		
-		contactBook.addContact(new Contact(1, Input.inputString("Name: "), 
-									Input.inputString("Surname: "),
-									phoneNumber.formatPhoneNumber(Input.inputString("Phone number: ")),
-									address.formatAddress(Input.inputString("Street: "), Input.inputString("Number: "),
-																					Input.inputString("Postal code: "))));
-		contactBook.addContact(new Contact(2, Input.inputString("Name: "), 
-				Input.inputString("Surname: "),
+
+		contactBook.addContact(new Contact(1, Input.inputString("Name: "), Input.inputString("Surname: "),
 				phoneNumber.formatPhoneNumber(Input.inputString("Phone number: ")),
 				address.formatAddress(Input.inputString("Street: "), Input.inputString("Number: "),
-																Input.inputString("Postal code: "))));
-		
-		
+						Input.inputString("Postal code: "))));
+		contactBook.addContact(new Contact(2, Input.inputString("Name: "), Input.inputString("Surname: "),
+				phoneNumber.formatPhoneNumber(Input.inputString("Phone number: ")),
+				address.formatAddress(Input.inputString("Street: "), Input.inputString("Number: "),
+						Input.inputString("Postal code: "))));
+
 		contactBook.printContactList();
-		
+
 		try {
 			contactBook.removeContact(2);
 		} catch (NoContactException e) {
 			System.err.println("Contact does not exist");
 			e.printStackTrace();
 		}
-		
+
 		contactBook.printContactList();
 
-		
 		/*
 		 * - Level 2 Abstract Factory** Create a small manager of international phone
 		 * numbers and addresses. The application must allow international addresses and
